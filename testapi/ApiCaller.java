76b74e3d329b733f1eeaf59873122737949bae90
BeXws <65669585+BeXws@users.noreply.github.com>
2024-05-13 14:51:24 +0200

    Add files via upload

---
+++ testapi/ApiCaller.java
@@ -0,0 +1,66 @@
+package testapi;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.net.URL;
+
+/**
+ * ApiCaller is the class that access BNF's API
+ * 
+ * @author TDLT
+ */
+public class ApiCaller {
+
+	/**
+	 * @param query this is the submitted query
+	 * @param start from which book do we want to start
+	 * @param number the number of book we want
+	 * @return a list of books the API returned
+	 */
+	public static List<Book> call(String query, int start, int number) {
+		// is appended to the query a link to the API, parameters to get only books,
+		// options to get the API's response in the expected format
+		String myURL = "https://catalogue.bnf.fr/api/SRU?version=1.2&operation=searchRetrieve&query=" + query
+				+ " and (bib.doctype any \"a\") &recordSchema=dublincore&maximumRecords="+number+"&startRecord="+start;
+		URI juri = null;
+
+		// this is to encode special characters in a usable format eg. " " gives "%20"
+		try {
+			URL url = new URL(myURL);
+			String nullFragment = null;
+			juri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), nullFragment);
+		} catch (MalformedURLException e) {
+			System.out.println("URL " + myURL + " is a malformed URL");
+		} catch (URISyntaxException e) {
+			System.out.println("URI " + myURL + " is a malformed URL");
+		}
+
+		// building the request
+		HttpRequest request = HttpRequest.newBuilder().uri(juri).method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = null;
+
+		// getting the response
+		try {
+			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// writing the response in a file and then parsing the file to get usable
+		// instances of books
+		OperaFile.createFile("answer.xml");
+		OperaFile.writeFile("answer.xml", response.body());
+		List<Book> books = Metamorph.createBook("answer.xml");
+
+		return books;
+	}
+}
