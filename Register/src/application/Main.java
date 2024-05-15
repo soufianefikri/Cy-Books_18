9c125841ac0c3cd3f03f0d0a2ebe8f2a1a780dfd
soufiane_fikri <105205326+soufianefikri@users.noreply.github.com>
2024-05-15 09:57:36 +0200

    Add files via upload

--- Register/src/application/Main.java
+++ Register/src/application/Main.java
@@ -11,12 +11,6 @@ import javafx.scene.image.*;
 
 
 public class Main extends Application {
-	
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-
 	@Override
 	public void start(Stage stage) throws Exception {
 		try {
@@ -24,15 +18,44 @@ public class Main extends Application {
 			Scene scene = new Scene(root,800,600); 
 			scene.getStylesheets().add(getClass().getResource("Register.css").toExternalForm());
 			stage.setTitle("Cy-Books_18 Register Users");
-			/*Image image = new Image("new_user.png");
-			ImageView imageView = new ImageView(image);
 			
-			Pane pane = (Pane) root;
-			root.getChildren().add(imageView);*/
+			
+			
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+
+	
 }
+
+
+
+
+
+/*@Override
+public void start(Stage stage) throws Exception {
+	try {
+		/*Parent root = FXMLLoader.load(getClass().getResource("/Register.fxml"));
+		Scene scene = new Scene(root,800,600); 
+		scene.getStylesheets().add(getClass().getResource("Register.css").toExternalForm());
+		stage.setTitle("Cy-Books_18 Register Users");
+		/*Image image = new Image("new_user.png");
+		ImageView imageView = new ImageView(image);
+		
+		Pane pane = (Pane) root;
+		root.getChildren().add(imageView);
+		stage.setScene(scene);
+		stage.show();
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+}*/
--- Register/src/module-info.java
+++ Register/src/module-info.java
@@ -1,6 +1,9 @@
 module Register {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
